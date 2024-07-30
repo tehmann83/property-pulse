bfb80d0b05e9247ddf6ac8ab649c180d692c0166
Tom Ehmann <ehmannth@gmail.com>
2024-07-30 20:05:14 +0200

    Edit property

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,10 @@
 import '@/assets/styles/globals.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 import AuthProvider from '@/components/AuthProvider';
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
+import { ToastContainer } from 'react-toastify';
 
 export const metadata = {
   title: 'PropertyPulse | Find The Perfect Rental',
@@ -18,6 +20,7 @@ const MainLayout = ({ children }) => {
           <Navbar />
           <main>{children}</main>
           <Footer />
+          <ToastContainer />
         </body>
       </html>
     </AuthProvider>
