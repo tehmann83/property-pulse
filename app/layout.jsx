d91b6d386d1c2306a59bfb88a887ab2b4a0c4b5d
Tom Ehmann <ehmannth@gmail.com>
2024-07-30 15:25:49 +0200

    Add user authentication

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import '@/assets/styles/globals.css';
 
+import AuthProvider from '@/components/AuthProvider';
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 
@@ -11,13 +12,15 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 
