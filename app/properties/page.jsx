112ab5480b826695aac071d6947dacd105254bc2
Tom Ehmann <ehmannth@gmail.com>
2024-07-31 11:20:37 +0200

    Add pagination

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,27 +1,48 @@
+import Pagination from '@/components/Pagination';
 import Property from '@/models/Property';
 import PropertyCard from '@/components/PropertyCard';
-import React from 'react';
+import PropertySearchForm from '@/components/PropertySearchForm';
 import connectDB from '@/config/database';
 
-const PropertiesPage = async () => {
+const PropertiesPage = async ({ searchParams: { pageSize = 9, page = 1 } }) => {
   await connectDB();
+  const skip = (page - 1) * pageSize;
 
-  const properties = await Property.find({}).lean();
+  const total = await Property.countDocuments({});
+  const properties = await Property.find({}).skip(skip).limit(pageSize);
+
+  // Calculate if pagination is needed
+  const showPagination = total > pageSize;
 
   return (
-    <section className="px-4 py-6">
-      <div className="container-xl lg:container m-auto px-4 py-6">
-        {properties.length === 0 ? (
-          <p>No properties found</p>
-        ) : (
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {properties.map((property) => (
-              <PropertyCard key={property._id} property={property} />
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
+    <>
+      <section className="bg-blue-700 py-4">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-start">
+          <PropertySearchForm />
+        </div>
+      </section>
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          <h1 className="text-2xl mb-4">Browse Properties</h1>
+          {properties.length === 0 ? (
+            <p>No properties found</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {properties.map((property, index) => (
+                <PropertyCard property={property} key={index} />
+              ))}
+            </div>
+          )}
+          {showPagination && (
+            <Pagination
+              page={parseInt(page)}
+              pageSize={parseInt(pageSize)}
+              totalItems={total}
+            />
+          )}
+        </div>
+      </section>
+    </>
   );
 };
 
